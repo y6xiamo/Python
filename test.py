55fe673b186c9fb8b1ab53f5e17548d586eb57e0
y6xiaomo <xiamo6512@163.com>
2018-07-23 12:14:58 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding=utf-8
+#print 'hello world'
+#
+#count = 10
+#count %= 10
+#print count
+
+#别名
+#a = 100
+#print a
+#
+#a = 'hehe'
+#print a
+
+#变量的类型
+#a = 1
+#print type(a)
+#
+#a = 1.0
+#print type(a)
+#
+#a = 'hahah'
+#print type(a)
+
+#python中变量的范围没有限制
+a = 1000000*1000000000*1000000000000*1000000000000
+print a
+
+#复数类型
+a = 1 + 5j
+print a
+
+#字符串可以用单引号（''），多引号(" ")"和(''' '''")表示
+a = 'heheh'
+b = "hahahha"
+c = '''hello'''
+print a 
+print b
+print c
+
